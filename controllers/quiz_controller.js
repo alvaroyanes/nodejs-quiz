ad46bb781a32ad0bae09adf5465b7ffcef024139
alvaroyanes <alv.yanes@gmail.com>
2015-08-03 22:32:30 +0200

    corrección errores y lista temática

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,7 +2,7 @@ var models = require('../models/models.js');
 
 // Autoload - factoriza el código si ruta incluye :quizId
 exports.load = function(req, res, next, quizId){
-    models.Quiz.find(quizId).then(
+    models.Quiz.findById(quizId).then(
         function(quiz){
             if(quiz){
                 req.quiz = quiz;
@@ -25,14 +25,14 @@ exports.index = function(req, res, next){
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-    models.Quiz.findOne(req.params.quizId).then(function(quiz) {
+    models.Quiz.findById(req.params.quizId).then(function(quiz) {
         res.render('quizes/show', { quiz: req.quiz});
     })
 };
 
 // GET /quizes/ :id/answer
 exports.answer = function(req, res) {
-    models.Quiz.findOne(req.params.quizId).then(function(quiz) {
+    models.Quiz.findById(req.params.quizId).then(function(quiz) {
         var resultado = 'Incorrecto';
         if (req.query.respuesta === req.quiz.respuesta){
               resultado = 'Correcto';
--- models/models.js
+++ models/models.js
@@ -20,13 +20,19 @@ exports.Quiz = Quiz;// exportar definición de la tabla Quiz
 sequelize.sync().then(function(){
 	//then(..) ejecuta el manejador una vez creada la tabla
 	Quiz.count().then(function(count){
-		if(count===0){//la tabla se inicializa solo si está vacía
-      Quiz.bulkCreate(
-        [ {pregunta: 'Capital de Italia',   respuesta: 'Roma'},
-          {pregunta: 'Capital de Portugal', respuesta: 'Lisboa'}
-        ]
-      ).then(function(){
-				console.log('Base de datos inicializada')
+		if(count===0){
+			Quiz.create({
+				pregunta: 'Cual es la capital de Italia',
+				respuesta: 'Roma',
+				tema: 'Geografía'
+			});
+			Quiz.create({
+				pregunta: 'Cual es la capital de Portugal',
+				respuesta: 'Lisboa',
+				tema: 'Turismo'
+			})
+	      	.then(function(){
+					console.log('Base de datos inicializada')
 			});
 		};
 	});
