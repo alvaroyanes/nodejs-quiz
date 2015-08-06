053077d22e0cee782908cc20e4def292ff129518
alvaroyanes <alv.yanes@gmail.com>
2015-08-06 21:19:34 +0200

    autorización acciones y rutas

--- routes/index.js
+++ routes/index.js
@@ -18,14 +18,14 @@ router.post('/login', sessionController.create);  // Crear sesión
 router.get('/logout', sessionController.destroy); // Eliminar sesión
 
 //definición de rutas de /quizes
-router.get('/quizes',                           quizController.index);
-router.get('/quizes/:quizId(\\d+)',             quizController.show);
-router.get('/quizes/:quizId(\\d+)/answer',      quizController.answer);
-router.get('/quizes/nuevo',                     quizController.new);
-router.post('/quizes/create',                   quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit',        quizController.edit);
-router.put('/quizes/:quizId(\\d+)',             quizController.update);
-router.delete('/quizes/:quizId(\\d+)',          quizController.destroy);
+router.get('/quizes',                       quizController.index);
+router.get('/quizes/:quizId(\\d+)',         quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',  quizController.answer);
+router.get('/quizes/nuevo',                 sessionController.loginRequired, quizController.new);
+router.post('/quizes/create',               sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',    sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)',         sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)',      sessionController.loginRequired, quizController.destroy);
 // Definición de ruta de author
 router.get('/author',                           quizController.author);
 
